24d53eba522668d3e7e776986fae38978d6ef789
Pedro H R C Tirelli <phtirelli@gmail.com>
2025-07-29 11:13:16 -0300

    função listar amigos

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaNomes = [];
 
-
+//Função para adicionar amigo
 function adicionarAmigo() {
     let input = document.getElementById("amigo").value;
 
     if (input === '') {
         alert('O campo não pode estar vazio. Adicione um nome');
+    } else if (listaNomes.includes(input)) {
+        alert('Nome já adicionado. Por favor, insira um nome diferente.');
+        document.getElementById('amigo').value = '';
     } else {
         listaNomes.push(input);
-        input = document.getElementById("amigo").value = '';
-        console.log(listaNomes);
-
+        listarAmigos();
+        document.getElementById('amigo').value = '';
     }
 
 
 }
 
+//função para listar amigos
+function listarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
 
+    for (let i = 0; i <= listaNomes.length; i++) {
+        let novoNome = document.createElement("li");
+        novoNome.textContent = listaNomes[i];
+        lista.appendChild(novoNome);
+    }
+
+
+}
